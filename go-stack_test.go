699f61886f6f336c08fe9eb50149f8655491e91c
Charlie Xu <cxu01@ukc02c9d76lvdl.fios-router.home>
2020-05-11 17:47:48 -0400

    Added test case TestAddOne

--- go-stack_test.go
+++ go-stack_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-stack/stack"
 	"testing"
 )
@@ -13,5 +14,30 @@ func TestNew(t *testing.T) {
 	}
 	if stk.Elements() != nil {
 		t.Errorf("got %v, want %v", stk.Len(), nil)
+	}
+}
+
+func TestAddOne(t *testing.T) {
+	var stk *stack.Stack = stack.New()
+	var firstElement *stack.Element = new(stack.Element)
+
+	expFirstValue := "firstElementTestValue"
+	expLen := 1
+	firstElement.Value = expFirstValue
+	stk.Add(firstElement)
+
+	if stk.Len() != expLen {
+		t.Errorf("got %v, want %v", stk.Len(), expLen)
+	}
+	if stk.Elements() == nil {
+		t.Errorf("got %v, want %v", stk.Len(), nil)
+	}
+
+	poppedElement := stk.Pop()
+	poppedValue := poppedElement.Value
+	fmt.Printf("first value was %v\n", poppedValue)
+	if poppedValue != expFirstValue {
+		t.Errorf("got first value %v, want %v", poppedElement.Value, expFirstValue)
+
 	}
 }
